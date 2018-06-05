244e6e265566194bb119380a35d9bd9d173aae19
Matheus Castro <goca1377@gmail.com>
2018-06-05 10:32:06 -0300

    Commit 4 modificação simples

--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,6 @@ import React from 'react'
 Functional Stateless Component
 Componente stateless não tem um estado.
 */
-const Comment = props => 
-    <div className="card"><p className="card-body">{props.comment.comment}</p></div>
+const Comment = props => <div className="card"><p className="card-body">{props.comment.comment}</p></div>
 
 export default Comment
